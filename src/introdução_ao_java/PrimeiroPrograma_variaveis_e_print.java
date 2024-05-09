f17de257ce277c860ba2c32241d997189920e353
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-09 15:34:01 -0300

    readequações

--- src/introdução_ao_java/PrimeiroPrograma_variaveis_e_print.java
+++ src/introdução_ao_java/PrimeiroPrograma_variaveis_e_print.java
@@ -1,34 +1,34 @@
 /*
- * Pacotes, ou packages, s�o uma forma de estrutura��o de programas Java
- * na qual facilita a organiza��o da constru��o do programa, pois assim
- * voc� consegue lidar com diferentes objetos e programas ao mesmo tempo
+ * Pacotes, ou packages, são uma forma de estruturação de programas Java
+ * na qual facilita a organização da construção do programa, pois assim
+ * você consegue lidar com diferentes objetos e programas ao mesmo tempo
  * em um projeto que seja mais extenso.
  */
 package introdução_ao_java;
 
 /*
- * Abaixo est� o objeto principal do prorama, ou seja, o "conjunto maior" do programa.
- * De maneira mais sint�tica, � como se houvesse um carro, o objeto principal. E isso
+ * Abaixo está o objeto principal do prorama, ou seja, o "conjunto maior" do programa.
+ * De maneira mais sintética, é como se houvesse um carro, o objeto principal. E isso
  * se divide em classes menores, como por exemplo o objeto pneu, vidro e afins,
  */
 
 public class PrimeiroPrograma_variaveis_e_print {
 /*
- * a classe main � basicamente onde se inicia o programa java, ou seja, �
- * a partir dali que o c�digo come�ar� a rodar. 
+ * a classe main é basicamente onde se inicia o programa java, ou seja, é
+ * a partir dali que o código começará a rodar. 
  */
 	public static void main(String[] args) {
 		/*
-		 * Abaixo s�o as vari�veis. Elas s�o espa�os da mem�ria designados a
-		 * armazenar dado. Estes dados podem ou n�o serem inseridos pelo usu�rio.
-		 * E estas vari�veis se dividem em primitivas ou n�o. As primitivas s�o:
-		 * int, long, short, byte (estas 4 armazenam n�meros inteiros), float,
+		 * Abaixo são as variáveis. Elas são espaços da memória designados a
+		 * armazenar dados. Estes dados podem ou não serem inseridos pelo usuário.
+		 * E estas variáveis se dividem em primitivas ou não. As primitivas são:
+		 * int, long, short, byte (estas 4 armazenam números inteiros), float,
 		 * double, estes dois armazenando numeros reais, e o tipo char
 		 */
 			int A = 20;
 			System.out.println(A);
 			/*
-			 * O comando acima exibe uma mensagem ou o valor de uma vari�ve�
+			 * O comando acima exibe uma mensagem ou o valor de uma variável, ou os dois.
 			 */
 	}
 
