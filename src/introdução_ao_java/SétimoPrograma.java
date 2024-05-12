8dcf317400f2427ccb854b66aea0b2dafc62d14b
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-11 22:19:29 -0300

    Adição daquilo que será a seção de comentários no sétimo programa

--- src/introdução_ao_java/SétimoPrograma.java
+++ src/introdução_ao_java/SétimoPrograma.java
@@ -1,23 +1,32 @@
 package introdução_ao_java;
 
 /*
- * 
+ * Comentários em construção
  */
 import java.util.Scanner;
 
 public class SétimoPrograma {
 
 	public static void main(String[] args) {
+		/*
+		 * Comentários em construção
+		 */
 		Scanner entrada = new Scanner(System.in);
 		int A=0;
 		int B=0;
 		int resultado=0;
+		/*
+		 * Comentários em construção
+		 */
 		System.out.println("Digite um numero:");
 		A=entrada.nextInt();
 		System.out.println("Digite outro numero:");
 		B=entrada.nextInt();
 		resultado=A+B;
 		System.out.println("A soma dos números digitados é:" + resultado);
+		/*
+		 * Comentários em construção
+		 */
 		entrada.close();
 	}
 
