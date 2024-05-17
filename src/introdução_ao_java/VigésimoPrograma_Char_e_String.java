c2e56c0fabf71f45092ece1130cb87076ea0df01
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-17 17:11:08 -0300

    Pequenas mudanças

---
+++ src/introdução_ao_java/VigésimoPrograma_Char_e_String.java
@@ -0,0 +1,11 @@
+package introdução_ao_java;
+
+public class VigésimoPrograma_Char_e_String {
+
+	public static void main(String[] args) {
+		String A = "Abecedário";
+		char primeiraLetra = A.charAt(1);
+		System.out.println(primeiraLetra);
+	}
+
+}
