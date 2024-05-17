db3f9a3090929b713632b10a348c6fcf459b396d
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-17 16:14:35 -0300

    Inserção de mais alguns programas

---
+++ src/introdução_ao_java/VigésimoPrograma_Char_E_String.java
@@ -0,0 +1,12 @@
+package introdução_ao_java;
+
+public class VigésimoPrograma_Char_E_String {
+
+	public static void main(String[] args) {
+		String Nome = "Jonas";
+		char PrimeiraLetra = Nome.charAt(0);
+		System.out.println(PrimeiraLetra);
+
+	}
+
+}
