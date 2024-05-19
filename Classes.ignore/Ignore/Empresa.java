9e1f069bc46abc0792903015eec2c0881e59de17
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-19 12:37:48 -0300

    pendente

--- Classes.ignore/Ignore/Empresa.java
+++ Classes.ignore/Ignore/Empresa.java
@@ -1,22 +1,24 @@
 package Ignore;
 
-public class Empresa {
 
+public class Empresa {
+	public String Nome = "";
+	public String CNPJ;
+	public int num_funcionarios;
 	public static void main(String[] args) {
-		String Nome;
-		String CNPJ;
-		int num_funcionarios = 60;
-		
-	}
-		public void demitir() {
-			int num_funcionarios = 60;
-			num_funcionarios--;
-			System.out.println("O numero de funcionarios caiu para: " + num_funcionarios);
-		}
+	        
+	    }
+  
+    public int demitir() {
+        int num_funcionarios = 60;
+        num_funcionarios--;
+        return num_funcionarios; 
+    }
 			public void contratar(){ 
 				int num_funcionarios = 60;
 				num_funcionarios++;
 				System.out.println("O numero de funcionarios aumentou para: " + num_funcionarios);
 				
 
-			}}
+			} 
+}
--- src/introduçãoAProgramaçãoOrientadaObjeta/TerceiroPrograma_objetos_fisicos.java
+++ src/introduçãoAProgramaçãoOrientadaObjeta/TerceiroPrograma_objetos_fisicos.java
@@ -1,11 +1,30 @@
+/*
+ * Código com problemas a ser resolvido
+ */
+
 package introduçãoAProgramaçãoOrientadaObjeta;
 import Ignore.Empresa;
-
+import java.util.Scanner;
 public class TerceiroPrograma_objetos_fisicos{
-
+	
 	public static void main(String[] args) {
+		int entrada1 = 0;
+		Scanner entrada = new Scanner (System.in);
 			Empresa empresa = new Empresa();
-			empresa.contratar();
-			empresa.demitir();
-	}}
-
+			empresa.num_funcionarios = empresa.demitir();
+			while(true) {
+			System.out.println("\n\tO que você deseja fazer? \n\t1:Demitir \n\t2:Contratar \n\tEntre com o numero");
+			entrada1 = entrada.nextInt();
+			if (entrada1 ==1) {
+			 empresa.demitir();
+			 System.out.println(empresa.num_funcionarios);
+			}
+			else if (entrada1==2) {
+				empresa.contratar();
+			}
+				if (entrada1!=1 && entrada1!=2){
+				System.out.println("insira algo válido fdp");
+				break;
+			}
+				
+	}entrada.close();}}
