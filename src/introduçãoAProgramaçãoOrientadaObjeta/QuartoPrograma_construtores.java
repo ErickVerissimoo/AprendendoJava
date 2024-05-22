9e02fdd0589844d22dfbb9bc4981faecbe2f4611
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-22 17:53:22 -0300

    construtor

---
+++ src/introduçãoAProgramaçãoOrientadaObjeta/QuartoPrograma_construtores.java
@@ -0,0 +1,16 @@
+package introduçãoAProgramaçãoOrientadaObjeta;
+import classes.Casa;
+
+public class QuartoPrograma_construtores{
+  
+	public static void main(String[] args) {
+		/*
+		 * Abaixo instanciamos o objeto casa 2 e chamamos o método construtor
+		 * contido no pacote "classes" que foi importado inicialmente em cima do arquivo.
+		 * 
+		 */
+	Casa casa2 = new Casa(22, 23);
+	System.out.println(casa2.altura);
+
+
+	}}
