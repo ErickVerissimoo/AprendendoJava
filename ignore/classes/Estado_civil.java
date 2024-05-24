baca60fc425c38da45d1000bcf209b733de1e225
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-24 17:58:51 -0300

    breve introdução ao enum

---
+++ ignore/classes/Estado_civil.java
@@ -0,0 +1,10 @@
+package classes;
+
+public enum Estado_civil {
+	solteiro,
+	casado,
+	divorciado,
+	viuvo,
+	namorando;
+}
+	
---
+++ src/introduçãoAProgramaçãoOrientadaObjeta/SétimoPrograma_enum.java
@@ -0,0 +1,15 @@
+package introduçãoAProgramaçãoOrientadaObjeta;
+import classes.Estado_civil;
+import classes.usuario;
+public class SétimoPrograma_enum {
+
+	public static void main(String[] args) {
+		usuario User = new usuario(1.80, 59, "Erick", true, true, Estado_civil.divorciado);
+		System.out.println(User.getEstado());
+		
+			
+			
+		}
+
+	}
+
