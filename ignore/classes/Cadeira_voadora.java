4af6c2d07dddfb3eaac9612210456deeb4f904c4
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-25 12:16:59 -0300

    Explicação sobre contrutores de maneira mais precisa e aprofundada

---
+++ ignore/classes/Cadeira_voadora.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class Cadeira_voadora {
+	private boolean voando = true;
+	private boolean rapido = true;
+	private boolean existe = false;
+	
+	public Cadeira_voadora() {
+		System.out.println(voando);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
