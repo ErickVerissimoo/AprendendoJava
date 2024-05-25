fbbb29c78ab2f8731ef650ee8cd310ca73de2dd0
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-25 20:24:55 -0300

    inicio da conceituação de interface (falta corrigir os problemas)

---
+++ ignore/classes/Jogos.java
@@ -0,0 +1,5 @@
+package classes;
+
+public interface Jogos {
+	public void Iniciar();
+}
---
+++ src/introduçãoAProgramaçãoOrientadaObjeta/DécimoSegundoPrograma_interface_intro.java
@@ -0,0 +1,16 @@
+package introduçãoAProgramaçãoOrientadaObjeta;
+import classes.Jogos;
+public class DécimoSegundoPrograma_interface_intro implements Jogos {
+	@Override
+	public void Iniciar() {
+		System.out.println("A");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
