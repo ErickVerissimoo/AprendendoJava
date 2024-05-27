3037ed87689121f0856d8daa23439b6d0bcc7c1f
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-05-27 18:18:02 -0300

    Inicio do mini projeto adicionado

--- Mini-Projetos/MiniProjetos/GerenciamentoEstoque.java
+++ Mini-Projetos/MiniProjetos/GerenciamentoEstoque.java
@@ -18,27 +18,27 @@ public class GerenciamentoEstoque {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		int opcao;
+		int i;
 		ArrayList <Produto> produtos = new ArrayList<>();
 		System.out.print("\n\tMenu do estoque \n\tO que você deseja fazer: \n\n\t1 - Adicionar produto \n\t2 - Alterar produtos \n\t3 - Consultar produtos específicos \n\t4 - Ver estoque \n\t5 - Sair \n\n\tEntre com a escolha: ");
 		opcao = entrada.nextInt();
 	
 		do {
 		Produto produto = new Produto();
-		double Preco = produto.getPreco();
-		double Quantidade = produto.getQuantidade();
-		String descricao = produto.getDescricao();
 		if (opcao == 1) {
-			System.out.println("\n\tVocê escolheu adicionar produto! \n\tEntre com o nome do produto: ");
+			System.out.print("\n\tVocê escolheu adicionar produto! \n\tEntre com o nome do produto: ");
 			produto.setNome(entrada.next());
-			System.out.println("\n\tEntre com a descrição do produto: ");
+			System.out.print("\n\tEntre com a descrição do produto: ");
 			produto.setDescricao(entrada.next());
-			System.out.println("\n\tEntre com o preço:");
+			System.out.print("\n\tEntre com o preço:");
 			produto.setPreco(entrada.nextDouble());
-			System.out.println("Entre com a quantidade");
+			System.out.print("\n\tEntre com a quantidade");
 			produto.setQuantidade(entrada.nextInt());
 			produtos.add(produto);
-		}
-		}while( opcao != 5);
-	}
-
-}
+			System.out.println("\n\tProduto adicionado com sucesso! \n\tVocê deseja cadastrar outro produto? \n\t1- sim \n\t2 - não");
+			if (opcao == 2) {
+			for (i=0; i>=produtos.size(); i++) {
+				System.out.println(produtos.get(i));
+			}
+		}}}while (opcao ==1);}}
+	
