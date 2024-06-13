73ffef832d68e0bbd2e375bbfc77aea185fd6f99
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-06-13 09:48:13 -0300

    metodo somar criado

--- Mini-Projetos/MiniProjetos/calculadora.java
+++ Mini-Projetos/MiniProjetos/calculadora.java
@@ -6,29 +6,37 @@ public class calculadora implements RegrasCalculadora {
 	static Scanner entrada = new Scanner(System.in);
 	static ArrayList <Double> numeros = new ArrayList <>();
 	static double Vetor [][] = new double[1000] [1000];
-	double a, b;
-	public void somar() {
-		System.out.print("Você escolheu somar! Entre com o primeiro numero  ");
-		a = entrada.nextDouble();
-		System.out.println("Entre com o segundo numero: ");
-		b = entrada.nextDouble();
-		Vetor [999][999] = a+b;
-		System.out.printf("\n\tA soma é %f ", a+b);
-		System.out.print( "\n\tVocê deseja continuar?" 
-				+ "\n\t1 - sim"
-				+ "\n\t2 - não" );
-		opcao = entrada.nextInt();
-		while(opcao==1) {
-		for (int i = 0; i<Vetor.length;i++) {
-			double c;
-			System.out.println("Entre com mais um número para ser adicionado: ");
-			c = entrada.nextInt();
-			Vetor [i][i] = c;
-			System.out.printf("A soma disso é: %f ",a+b+c);
-		}
-		}
+	double a, b, c;
+
+	    public void somar() {
+	        double soma = 0;
+
+	        System.out.print("Você escolheu somar! Entre com o primeiro número: ");
+	        double a = entrada.nextDouble();
+
+	        System.out.print("Entre com o segundo número: ");
+	        double b = entrada.nextDouble();
+
+	        soma = a + b;
+	        System.out.printf("\nA soma é %.2f%n", soma);
+
+	        int opcao;
+	        do {
+	            System.out.print("\nVocê deseja continuar?" +
+	                    "\n1 - sim" +
+	                    "\n2 - não: ");
+	            opcao = entrada.nextInt();
+
+	            if (opcao == 1) {
+	                System.out.print("Entre com mais um número para ser adicionado: ");
+	                double c = entrada.nextDouble();
+	                soma += c;
+	                System.out.printf("A soma disso é: %.2f%n", soma);
+	            }
+	        } while (opcao == 1);
+	    }
+
 		
-	}
 
 	public void subtrair() {
 		// TODO Auto-generated method stub
@@ -59,7 +67,4 @@ public class calculadora implements RegrasCalculadora {
 		calc.somar();
 
 	}
-
-	
-
 }
