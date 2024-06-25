848e5abede3f0cee610d3f4db76983bcb577ae4f
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-06-25 19:53:28 -0300

    conclusão

--- Mini-Projetos/MiniProjetos/adivinharjogo.java
+++ Mini-Projetos/MiniProjetos/adivinharjogo.java
@@ -7,13 +7,23 @@ public class adivinharjogo {
 		Scanner entrada = new Scanner (System.in);
 		int a = aleatorio.nextInt(100);
 		int b;
+		int tentativas = 10;
 		try {
 		System.out.println("Bem vindo ao jogo de adivinhar um numero"
 				+ "\n\tEntre com um numero inteiro até 100:");
 		b = entrada.nextInt();
-		while(b!=a) {
-			System.out.println("Palavra errada! Tente novamente \n\tDigite: ");
+		while(b!=a || tentativas>0) {
+			System.out.printf("Palavra errada! Lhe restam %d tentativas \n\tDigite: ", tentativas);
 			b = entrada.nextInt();
+			tentativas--;
+			if(tentativas<=0) {
+				System.out.printf("\tO numero era %d", a);
+				System.out.print("\n\tGame over!");
+				break;
+			}
+			if (b==a) {
+				System.out.print("\n\tVocê acertou o numero!");
+			}
 		}
 	}
 		catch(Throwable ex) {
