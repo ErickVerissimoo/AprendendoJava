e5c12b11975e31e7105cac30d007d62959cc59e1
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-06-25 21:48:44 -0300

    projeto em construção

--- Mini-Projetos/MiniProjetos/geradorEmail.java
+++ Mini-Projetos/MiniProjetos/geradorEmail.java
@@ -2,6 +2,8 @@ package MiniProjetos;
 import java.util.*;
 public class geradorEmail {
 	public static void Gerar() {
+		Random carac = new Random();
+		int c = carac.nextInt(17);
 		int i = 0;
 		String caracteres = "AhbcJNDAJSBoa asdkbJNJASND-IWQ9I3480938%r(&@*&@(*@)(NDNLXCDOHF]~ÇQP2=02475HD*-";
 		Random senha = new Random();
@@ -14,15 +16,22 @@ public class geradorEmail {
 		System.out.println("Digite o ano de nascimento:");
 		int ano = entrada.nextInt();
 		System.out.printf("\n\temail: %s%s%d@empresa.com", nome, sobrenome, ano);
-		while(i<10) {
-			Random carac = new Random();
-			int c = carac.nextInt();
-			c = caracteres.length();
-			caracteres.length();
-			i++;
+			while(i<10) {
+				++i;
+				System.out.printf("\n\tA sua senha é: %s%s%s%s%s%s%s%s%s%s"
+						+ "%s%s%s%s%s%s%s",caracteres.charAt(c+i),
+						caracteres.charAt(c+i),caracteres.charAt(c+i),
+						caracteres.charAt(c),caracteres.charAt(c+i),
+						caracteres.charAt(c),caracteres.charAt(c),
+						caracteres.charAt(c), caracteres.charAt(c),
+						caracteres.charAt(c), caracteres.charAt(c),
+						caracteres.charAt(c), caracteres.charAt(c),
+						caracteres.charAt(c), caracteres.charAt(c),
+						caracteres.charAt(c), caracteres.charAt(c)
+								
+						);
+			}
 		}
-		System.out.print("A sua senha é:" + caracteres);
-	}
 	public static void main(String[] args) {
 		Gerar();
 	}
