9cc7a49bfa3cfa2b9e8bc89644e92b116cbe2b0d
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-06-30 16:50:40 -0300

    prototipagem inicial

--- src/prototipagem/metodos.java
+++ src/prototipagem/metodos.java
@@ -10,49 +10,59 @@ import java.util.*;
  * @author Erick
  */
 public abstract class metodos {
-    public static String[] palavras = {"\n\tMacaco", "\n\tElefante", "\n\tCachorro", "\n\tGirafa", "\n\tVaca"}; 
+    public static String[] palavras = {"Macaco", "Elefante", "Cachorro", "Girafa", "Vaca"}; 
         public static void jogo(){
-        try{
-        Random escolha = new Random();
-        int aleatorio = escolha.nextInt(0, 5);
-            Scanner entrada = new Scanner (System.in);
-            System.out.println("\n\tBem vindo ao jogo da forca! \n\tTema do jogo: animais");
-            for(int letra = 0; letra<palavras[aleatorio].length(); letra++){
-                System.out.print("\t _ ");
-        }
-            int tentativas = palavras[aleatorio].length() +3;
-            while(tentativas>0){
-            char letra;
-            System.out.print("\n\tEntre com uma letra da palavra: ");
-            letra = entrada.next().charAt(0);
-             
-              for(int index = 0; index<palavras[aleatorio].length(); index++ ){
-               if(letra == palavras[aleatorio].charAt(index) && index>0){
-                    System.out.print(Character.toLowerCase(letra));
-                    tentativas--;
-                } else{
-                   System.out.print("\n\t voce errou ");
-                   tentativas--;
-
-        }
-              
-                
-                
-                
-          if(letra == palavras[aleatorio].charAt(0)){
-               System.out.print(Character.toUpperCase(letra));}
-               else {
-        
-                System.out.print(" _ ");        
-          }
-            }} 
-            
-     
-        } catch(Throwable errado){
-            System.out.print("\n\tErro!");
-        }
+            boolean acabou = false;
+       try{
+           
+           int i = 0;
+           int tentativas = 15;
+           Scanner entrada = new Scanner (System.in);
+           Random aleatorio = new Random();
+           int random = aleatorio.nextInt(0, 5); 
+           System.out.print("\n\tBem vindo ao jogo da forca! \n\n\tDica: animais ");
+           System.out.println("                  ");
+           while(tentativas>0){
+               char letra;
+               while(i<palavras[random].length()){
+                   System.out.print(" _ ");
+                   i++;
+               }
+               System.out.println("           ");
+               System.out.print("\n\tEntre com uma letra: ");
+               letra = entrada.next().charAt(0);
+              if (Character.toUpperCase(letra) == palavras[random].charAt(0)) {
+    System.out.print("\n\tVocê acertou a primeira letra!");
+    String[] linhas = new String[palavras[random].length()];
+    String letrae = String.valueOf(Character.toUpperCase(letra));
+    linhas[0] = letrae;
+    System.out.print(letrae);
+    for (int in = 0; in < linhas.length; in++) {
+        System.out.print("\t _ ");
+    }
+} else if (Character.toUpperCase(letra) != palavras[random].charAt(0)) {
+    while (i < palavras[random].length()) {
+        System.out.print(" _ ");
+        i++;
+    }
+    System.out.print("\n\tVocê errou! \n\tTente novamente: ");
+    tentativas--;
 }
-    public static void main(String[] args) {
+
+
+           tentativas--;
+           }    
+       }catch(Throwable exception){  
+           System.out.println("Erro!");}
+    finally{
+            
+            }
+       }
+       
+           
+       
+        
+        public static void main(String[] args) {
         jogo();
     }
-} 
+}
