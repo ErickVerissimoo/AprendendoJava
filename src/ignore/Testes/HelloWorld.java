ca673119db852d3063afcb0a62405c01407382f2
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-07-05 19:08:50 -0300

    testes

--- src/ignore/Testes/HelloWorld.java
+++ src/ignore/Testes/HelloWorld.java
@@ -1,19 +1,22 @@
 package ignore.Testes;
 
-public final class HelloWorld{
+public final class HelloWorld {
 	public static String helloworld() {
 		String helloworld = "hello world";
-		
 		return helloworld;
 	}
+
 	public static void main(String[] args) {
-		
-		 String helloworld = helloworld();
-		 char [] helloworlds = helloworld.toCharArray();
-		 for(int i = 0; i<helloworlds.length; i++) {
-			 if(i%2 == 0) {
-		 System.out.print(helloworld.toUpperCase().charAt(i));
-	} else{
-		 System.out.print(helloworld.toLowerCase().charAt(i));
+
+		String helloworld = helloworld();
+		for (int i = 0; i < helloworld.length(); i++) {
+			if (i % 2 == 0) {
+				helloworld.trim();
+				System.out.print(helloworld.trim().toUpperCase().charAt(i));
+			} else {
+				System.out.print(helloworld.trim().toLowerCase().charAt(i));
+			}
+		}
 	}
-}}}
+	
+}
