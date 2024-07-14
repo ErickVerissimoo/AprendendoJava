93f7cc0d68cc19c5f6e0bbbac920da50a6a64488
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-07-14 20:19:38 -0300

    metade do jogo concluida

--- src/prototipagem/JogoDaVelha.java
+++ src/prototipagem/JogoDaVelha.java
@@ -5,19 +5,17 @@ public abstract class JogoDaVelha {
 	static char [][] tabuleiro = new char [3][3];
 	static boolean jogoAcabou = false;
 	static Scanner entrada = new Scanner (System.in);
-
+	
 	public static void Imprimir() {
-		
 		for(int i = 0; i<tabuleiro.length;i++) {
 		System.out.println("  |  |  | ");
 	}}
 		public static void Jogo() {
-
+			try {
 				Imprimir();
 				int maquinaVertical, maquinaHorizontal;
 				int horizontal;
 				int vertical;
-				int contador= 0;
 				char jogador;
 				Random aleatorio;
 				boolean terminou = false;
@@ -25,55 +23,76 @@ public abstract class JogoDaVelha {
 				boolean começa;
 				começa = aleatorio.nextBoolean();
 		
-				while(!terminou || começa == true) { 
+				do { 
 					jogador = 'X';
 					char maquina = 'O';
-					System.out.print("\n\tDigite a linha vertical de um a 3: ");
+					System.out.print("\n\tDigite a linha horizontal de um a 3: ");
 					horizontal = entrada.nextInt() -1;
-					System.out.print("\n\tEntre com a coluna horizontal de 1 a 3:");
+					System.out.print("\n\tEntre com a coluna vertical de 1 a 3:");
 					vertical = entrada.nextInt() -1;
-					tabuleiro [vertical][horizontal] = jogador;
+					tabuleiro [horizontal][vertical] = jogador;
 					maquinaVertical = aleatorio.nextInt(0, 3);
 					maquinaHorizontal = aleatorio.nextInt(0, 3);
-					while(tabuleiro [maquinaVertical][maquinaHorizontal] == tabuleiro [vertical][horizontal]);{
+					while(tabuleiro [maquinaVertical][maquinaHorizontal] == tabuleiro [vertical][horizontal]){
 						maquinaVertical = aleatorio.nextInt(0, 3);
 						maquinaHorizontal = aleatorio.nextInt(0, 3);
+			
 					}
 						 tabuleiro [maquinaVertical][maquinaHorizontal] = maquina;
-						
+					
 					for(int i = 0; i<tabuleiro.length; i++) {
 						for(int j = 0; j<tabuleiro.length; j++) {
-							   if (i == 0 && j == 2 && tabuleiro [i][j] == '\u0000') {
-									  System.out.print(" |");
+							
+							  if(tabuleiro[i][j] == '\u0000') {
+									System.out.print(" |");}
+							  if (i == 0 && j == 2 && tabuleiro [i][j] == '\u0000') {
+									 
 									  System.out.println();
 								  }
-							   else  if(tabuleiro[i][j] == '\u0000') {
-								System.out.print(" |");}
+							  else   if (i == 1 && j == 2 && tabuleiro [i][j] == '\u0000') {
+									
+									  System.out.println();
+								  }
 							
-								  if(i == 0 && j ==2 && tabuleiro[i][j] == jogador) {
+							   if(i == 0 && j ==2 && tabuleiro[i][j] == jogador) {
 									 System.out.printf(" %c",jogador );
 									 System.out.println();
 									
 								 }
-								  else if (tabuleiro[i][j] == jogador) {
-											System.out.printf(" %c",jogador );
-										}
-								  if(i == 1 && j == 2) {
-									  System.out.println();
-								  }
-								 if(i == 1 && j == 2 && tabuleiro[i][j] == jogador ) {
+							   else  if(i == 1 && j == 2 && tabuleiro[i][j] == jogador ) {
 									 System.out.printf(" %c",jogador );
 									 System.out.println();
 							} 
 							
-						
-							  if(tabuleiro [i][j] == maquina) {
-								System.out.printf(" %c", maquina);
+							   else if (tabuleiro[i][j] == jogador) {
+									System.out.printf(" %c",jogador );
+								}
+							   
+							   
+								 
+							   if(i == 0 && j == 2 && tabuleiro[i][j] == maquina ) {
+									 System.out.printf(" %c",maquina );
+									 System.out.println();
+							}
+							   else if(i == 1 && j == 2 && tabuleiro[i][j] == maquina ) {
+									 System.out.printf(" %c",maquina );
+									 System.out.println();
 							}
 						
+							   else if(tabuleiro [i][j] == maquina) {
+								System.out.printf(" %c", maquina);
+							}
+							  if (i == tabuleiro.length) {
+								i -= tabuleiro.length;
+							}
+							if (j == tabuleiro.length){
+								j -= tabuleiro.length;
+							}
 							  }
-						
-						
-						  }}	  
+						  }}while(terminou ==false);	  
 					 
-				}}
+				}catch(IndexOutOfBoundsException entradaInvalida){
+					System.out.print("\n\tentrada invalida");
+				}
+			
+		}}
