3a90aead49904f9d512d0b3f1e8d04237ab4adcf
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-07-21 22:16:02 -0300

    finalização

--- src/MiniProjetos/tabuleiro.java
+++ src/MiniProjetos/tabuleiro.java
@@ -13,7 +13,7 @@ public final class tabuleiro {
 		Scanner entrada = new Scanner(System.in);
 
 		while (repeticao == false) {
-			
+
 			Random aleatorio = new Random();
 
 			int Horizontal;
@@ -32,7 +32,8 @@ public final class tabuleiro {
 			maquina_vertical = aleatorio.nextInt(0, 3);
 			maquina_horizontal = aleatorio.nextInt(0, 3);
 
-			while (tabuleiro[maquina_horizontal][maquina_vertical] == jogada || tabuleiro[maquina_horizontal][maquina_vertical] == maquina ) {
+			while (tabuleiro[maquina_horizontal][maquina_vertical] == jogada
+					|| tabuleiro[maquina_horizontal][maquina_vertical] == maquina) {
 				aleatorio = new Random();
 				maquina_vertical = aleatorio.nextInt(0, 3);
 				maquina_horizontal = aleatorio.nextInt(0, 3);
@@ -40,16 +41,80 @@ public final class tabuleiro {
 			}
 
 			tabuleiro[maquina_horizontal][maquina_vertical] = maquina;
+
+			if (tabuleiro[0][0] == jogada && tabuleiro[1][0] == jogada && tabuleiro[2][0] == jogada) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê venceu!");
+				break;
+			} else if (tabuleiro[0][0] == jogada && tabuleiro[0][1] == jogada && tabuleiro[0][2] == jogada) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê venceu!");
+				break;
+
+			} else if (tabuleiro[0][0] == jogada && tabuleiro[1][1] == jogada && tabuleiro[2][2] == jogada) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê venceu!");
+				break;
+			} else if (tabuleiro[1][0] == jogada && tabuleiro[1][1] == jogada && tabuleiro[1][2] == jogada) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê venceu!");
+				break;
+			} else if (tabuleiro[2][0] == jogada && tabuleiro[2][1] == jogada && tabuleiro[2][2] == jogada) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê venceu!");
+				break;
+			} else if (tabuleiro[0][2] == jogada && tabuleiro[1][2] == jogada && tabuleiro[2][2] == jogada) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê venceu!");
+				break;
+			} else if (tabuleiro[2][0] == jogada && tabuleiro[1][1] == jogada && tabuleiro[0][2] == jogada) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê venceu!");
+				break;
+			} else if (tabuleiro[0][1] == jogada && tabuleiro[1][1] == jogada && tabuleiro[2][1] == jogada) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê venceu!");
+				break;
+			}
+
+			if (tabuleiro[0][0] == maquina && tabuleiro[1][0] == maquina && tabuleiro[2][0] == maquina) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê perdeu!");
+				break;
+			} else if (tabuleiro[0][0] == maquina && tabuleiro[0][1] == maquina && tabuleiro[0][2] == maquina) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê perdeu!");
+				break;
+
+			} else if (tabuleiro[0][0] == maquina && tabuleiro[1][1] == maquina && tabuleiro[2][2] == maquina) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê perdeu!");
+				break;
+			} else if (tabuleiro[1][0] == maquina && tabuleiro[1][1] == maquina && tabuleiro[1][2] == maquina) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê perdeu!");
+				break;
+			} else if (tabuleiro[2][0] == maquina && tabuleiro[2][1] == maquina && tabuleiro[2][2] == maquina) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê perdeu!");
+				break;
+			} else if (tabuleiro[0][2] == maquina && tabuleiro[1][2] == maquina && tabuleiro[2][2] == maquina) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê perdeu!");
+				break;
+			} else if (tabuleiro[2][0] == maquina && tabuleiro[1][1] == maquina && tabuleiro[0][2] == maquina) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê perdeu!");
+				break;
+			} else if (tabuleiro[0][1] == maquina && tabuleiro[1][1] == maquina && tabuleiro[2][1] == maquina) {
+				Interface(jogada, maquina);
+				System.out.print("\n\tVocê perdeu!");
+				break;
+			}
+
 			for (int vertical = 0; vertical < tabuleiro.length; vertical++) {
 				for (int horizontal = 0; horizontal < tabuleiro.length; horizontal++) {
 
-					if(vertical == tabuleiro.length ) {
-						vertical -=tabuleiro.length;
-					}
-					if(horizontal == tabuleiro.length) {
-						horizontal -= tabuleiro.length;
-					}
-					
 					if (horizontal == 2 && vertical == 0 && tabuleiro[horizontal][vertical] == null
 							|| horizontal == 2 && vertical == 1 && tabuleiro[horizontal][vertical] == null) {
 						System.out.print(jogada.getVazio());
@@ -63,8 +128,6 @@ public final class tabuleiro {
 					}
 					if (horizontal == 2 && vertical == 0 && tabuleiro[horizontal][vertical] == maquina
 							|| horizontal == 2 && vertical == 1 && tabuleiro[horizontal][vertical] == maquina
-							
-							
 
 					) {
 						System.out.printf(" %s", maquina.getMaquina());
@@ -106,6 +169,44 @@ public final class tabuleiro {
 
 		}
 
+	}
+
+	// @Overload
+	public static void Interface(Jogada jogada, Jogada maquina) {
+		for (int vertical = 0; vertical < tabuleiro.length; vertical++) {
+			for (int horizontal = 0; horizontal < tabuleiro.length; horizontal++) {
+
+				if (horizontal == 2 && vertical == 0 && tabuleiro[horizontal][vertical] == null
+						|| horizontal == 2 && vertical == 1 && tabuleiro[horizontal][vertical] == null) {
+					System.out.print(jogada.getVazio());
+					System.out.println();
+
+				}
+
+				else if (tabuleiro[horizontal][vertical] != maquina && tabuleiro[horizontal][vertical] != jogada) {
+
+					System.out.print(jogada.getVazio());
+				}
+				if (horizontal == 2 && vertical == 0 && tabuleiro[horizontal][vertical] == maquina
+						|| horizontal == 2 && vertical == 1 && tabuleiro[horizontal][vertical] == maquina
+
+				) {
+					System.out.printf(" %s", maquina.getMaquina());
+					System.out.println();
+				} else if (tabuleiro[horizontal][vertical] == maquina
+						|| tabuleiro[horizontal][vertical] != null && tabuleiro[horizontal][vertical] != jogada) {
+					System.out.printf(" %s", maquina.getMaquina());
+				}
+
+				if (horizontal == 2 && vertical == 0 && tabuleiro[horizontal][vertical] == jogada
+						|| horizontal == 2 && vertical == 1 && tabuleiro[horizontal][vertical] == jogada) {
+					System.out.printf(" %s", jogada.getJogada());
+					System.out.println();
+				} else if (tabuleiro[horizontal][vertical] == jogada) {
+					System.out.printf(" %s", jogada.getJogada());
+				}
+			}
+		}
 	}
 
 	public static void main(String... args) {
